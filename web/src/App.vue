bd82d40e470604bf0ae62cd8a7298f3faba1c824
null122 <1229608477@qq.com>
2025-06-08 17:39:15 +0800

    feat: 引入Element Plus和更新App layout

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,67 +1,55 @@
 <template>
+  <el-menu default-active="1">
+    <el-menu-item index="1">
+        <el-icon>
+          <location/>
+        </el-icon>
+        <span>首页</span>
+    </el-menu-item>
 
+    <el-menu-item index="2">
+        <el-icon>
+          <icon-menu/>
+        </el-icon>
+        <span>设备管理</span>
+    </el-menu-item>
+
+    <el-menu-item index="3">
+      <el-icon>
+        <document/>
+      </el-icon>
+      <span>预警管理</span>
+    </el-menu-item>
+
+    <el-sub-menu index="4">
+      <template #title>
+        <el-icon>
+          <icon-menu/>
+        </el-icon>
+        <span>系统管理</span>
+      </template>
+      <el-menu-item index="4-1">用户管理</el-menu-item>
+      <el-menu-item index="4-2">用户操作记录</el-menu-item>
+      <el-menu-item index="4-3">设备资产管理</el-menu-item>
+    </el-sub-menu>
+
+  </el-menu>
 </template>
 
+<script lang="ts" setup>
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+
+</script>
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.el-menu--horizontal {
+  --el-menu-horizontal-height: 100px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -2,10 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.mount('#app')
