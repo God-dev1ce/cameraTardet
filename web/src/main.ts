6393abf53b1b409c25ef5234fcf55f3ba8558369
null122 <1229608477@qq.com>
2025-06-13 18:55:30 +0800

    feat: 添加ECharts组件支持，更新依赖包

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,6 +6,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/assets/styles/global.css'
 import router from './router'
+import Echarts from 'vue-echarts'
 import 'echarts';
 
 const app = createApp(App)
@@ -15,5 +16,5 @@ app.use(ElementPlus)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
-
+app.component('ECharts', Echarts)
 app.mount('#app')
